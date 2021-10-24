f792c5b8943aec60b9e48c2c6e322e4e7a8d6d6d
apuniello <apuniello@gmail.com>
2021-10-24 13:15:26 -0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,27 @@
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
+
+(function () {
+        // Obtengo todos los elementos del form que requieren validación
+        var forms = document.querySelectorAll('.needs-validation')
+
+        // Armamos un loop sobre los elementos y prevenimos que se manden de estar mal.
+        Array.prototype.slice.call(forms)
+            .forEach(function (form) {
+                form.addEventListener('submit', function (event) {
+                    if (!form.checkValidity()) {
+                        event.preventDefault()
+                        event.stopPropagation()
+                    }
+
+                    form.classList.add('was-validated')
+                }, false)
+            })
+})()
